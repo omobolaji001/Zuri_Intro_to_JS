cfe06dbfd4705bdeddad6049cb79105183ce9df9
Omobolaji001 <omobolajiolayiwola11@gmail.com>
2022-06-23 18:35:02 +0100

    Added Project

---
+++ Calculator/intro.js
@@ -0,0 +1,9 @@
+// Simple Calculator
+function calc(num1, num2){
+  sum = num1 + num2;
+  difference = num1 - num2;
+  multiplication = num1 * num2;
+  division = num1 / num2;
+  
+  return `sum = ${sum} \n difference = ${difference} \n multiplication = ${multiplication} \n division = ${division}`
+}
---
+++ Profile/Intro.js
@@ -0,0 +1,3 @@
+//Simple Profile
+
+alert("Name: Oyebode Habeeb Omobolaji \n Height: 6'7 \n Country: Nigeria");
